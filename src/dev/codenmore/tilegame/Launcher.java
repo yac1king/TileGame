5bf7e6e7ffb368feef65099869190e54a0ab4f6b
yac1king <40617080+yac1king@users.noreply.github.com>
2018-12-10 11:17:21 +0800

    Initial commit

---
+++ src/dev/codenmore/tilegame/Launcher.java
@@ -0,0 +1,12 @@
+package dev.codenmore.tilegame;
+
+import dev.codenmore.tilegame.display.Display;
+
+public class Launcher {
+
+	public static void main(String[] args) {
+		Game game = new Game("¼ÐÃD", 400, 400);
+		game.start();
+	}
+
+}
